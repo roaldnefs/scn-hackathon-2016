1249f1f1cbec03ef8ffe79936f83bedc52801303
Roald Nefs <roowieee@gmail.com>
2016-05-12 19:01:21 +0200

    added some templates

--- bitwatch/pricewatch/urls.py
+++ bitwatch/pricewatch/urls.py
@@ -2,5 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^pricewatch$', views.pricewatch, name='pricewatch'),
+
+    url(r'^companies$', views.companies, name='companies'),
+    url(r'^company/(?P<id>[0-9]+)$', views.company, name='company'),
+
+    url(r'^product/(?P<id>[0-9]+)$', views.product, name='product'),
+
+    url(r'^about$', views.about, name='about'),
     url(r'^', views.index, name='index'),
+
 ]
--- bitwatch/pricewatch/views.py
+++ bitwatch/pricewatch/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import HttpResponse, render
 
+
 def index(request):
     return render(request, 'index.html', {})
+
+
+def pricewatch(request):
+    return render(request, 'pricewatch.html', {})
+
+
+def companies(request):
+    return render(request, 'companies.html', {})
+
+
+def company(request, id=None):
+    return render(request, 'company.html', {})
+
+
+def product(request, id=None):
+    return render(request, 'product.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
